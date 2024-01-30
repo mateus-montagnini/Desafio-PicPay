4a0ccf9e6b2ec1001dd1ac5f21f05352dde9e304
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-01-30 17:46:29 -0300

    Primeiro commit

---
+++ Desafio-PicPay/src/test/java/com/mrocha/br/BrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrocha.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
