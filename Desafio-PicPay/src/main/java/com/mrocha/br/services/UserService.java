2fc463ce2b48df957d3d02ef3027bb6e7216dffa
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-01-31 18:32:58 -0300

    Finalizando a classe de UserService

--- Desafio-PicPay/src/main/java/com/mrocha/br/services/UserService.java
+++ Desafio-PicPay/src/main/java/com/mrocha/br/services/UserService.java
@@ -10,4 +10,24 @@ import java.math.BigDecimal;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction (User sender, BigDecimal amount) throws Exception {
+        if(sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuario do tipo lojista nao esta autorizado a realizar transacao");
+        }
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuario nao encontrado"));
+    }
+
+    public void saveUser(User user) {
+        this.repository.save(user);
+    }
 }
