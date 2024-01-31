af45cd99192faa7212d4b182b5f14b2a0e818ffe
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-01-31 18:32:34 -0300

    Criando a classe de UserService

---
+++ Desafio-PicPay/src/main/java/com/mrocha/br/services/UserService.java
@@ -0,0 +1,13 @@
+package com.mrocha.br.services;
+
+import com.mrocha.br.domain.user.User;
+import com.mrocha.br.domain.user.UserType;
+import com.mrocha.br.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+}
