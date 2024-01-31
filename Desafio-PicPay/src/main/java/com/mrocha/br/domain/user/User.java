3f045674a341000cd08dbce514c0ac8b62612fcb
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-01-31 16:51:20 -0300

    Criando os parametros do domain user

--- Desafio-PicPay/src/main/java/com/mrocha/br/domain/user/User.java
+++ Desafio-PicPay/src/main/java/com/mrocha/br/domain/user/User.java
@@ -13,5 +13,17 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(of = "id")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
 }
