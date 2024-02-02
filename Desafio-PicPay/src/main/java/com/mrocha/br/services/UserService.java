ef7af5c5a70d5fad72f23e503416f8666552641c
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-02 16:43:14 -0300

    Finalizando o UserService

--- Desafio-PicPay/src/main/java/com/mrocha/br/services/UserService.java
+++ Desafio-PicPay/src/main/java/com/mrocha/br/services/UserService.java
@@ -2,11 +2,13 @@ package com.mrocha.br.services;
 
 import com.mrocha.br.domain.user.User;
 import com.mrocha.br.domain.user.UserType;
+import com.mrocha.br.dto.UserDTO;
 import com.mrocha.br.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -25,6 +27,16 @@ public class UserService {
 
     public User findUserById(Long id) throws Exception {
         return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuario nao encontrado"));
+    }
+
+    public User createUser(UserDTO data) {
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.repository.findAll();
     }
 
     public void saveUser(User user) {
